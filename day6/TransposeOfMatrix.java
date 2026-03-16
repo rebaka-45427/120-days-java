dc8e98ee8582bde74cc9872ce67f8545f8726dea
Rebaka Jesi <frebakajesi@gmail.com>
2026-03-16 19:46:00 +0530

    sum of matrixes

---
+++ day6/TransposeOfMatrix.java
@@ -0,0 +1,29 @@
+package day6;
+import java.util.Scanner;
+public class TransposeOfMatrix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] matrix = new int[3][3];
+        System.out.println("Enter 9 elements for the 3x3 matrix:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("The original Matrix is:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("The transpose Matrix is:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(matrix[j][i] + " ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
