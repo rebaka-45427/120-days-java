8e1ee4b166c665438bb76f5a117d905526a55e1d
Rebaka Jesi <frebakajesi@gmail.com>
2026-03-16 19:57:58 +0530

    completed day 6 tasks of 3dimentional array

---
+++ day6/DiagonalElement.java
@@ -0,0 +1,30 @@
+package day6;
+import java.util.Scanner;
+public class DiagonalElement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] matrix = new int[3][3];
+        System.out.println("Enter 9 elements for the 3x3 matrix:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("The 3x3 Matrix is:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("The Diagonal is: ");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if(matrix[i][j]==matrix[j][i]){
+                    System.out.print(matrix[i][j] + " ");
+                }
+            }
+        }
+        sc.close();
+    }
+}
