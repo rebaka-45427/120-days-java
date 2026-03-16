1fc4385650867713d091eda8ea313a77d389c406
Rebaka Jesi <frebakajesi@gmail.com>
2026-03-16 19:49:02 +0530

    multiply 2 matrix

---
+++ day6/Multiply2Matrix.java
@@ -0,0 +1,29 @@
+package day6;
+import java.util.Scanner;
+public class Multiply2Matrix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] matrix1 = new int[3][3];
+        int[][] matrix2 = new int[3][3];
+        System.out.println("Enter 9 elements for the 3x3 matrix1:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                matrix1[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("Enter 9 elements for the 3x3 matrix2:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                matrix2[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("The 3x3 Matrix is:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(matrix1[i][j] * matrix2[i][j]+ " ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
